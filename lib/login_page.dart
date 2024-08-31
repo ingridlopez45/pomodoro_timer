2816bc7ffaa8674105720f78136a2a16ef54a82d
ingridlopez45 <ilopezo3@miumg.edu.gt>
2024-08-30 23:46:55 -0600

    feat: Add InputText, Button, and TextAlignmentLeft components
    The code changes include the addition of three new components: InputText, Button, and TextAlignmentLeft. These components are essential for building the login page.
    
    The InputText component provides a text input field with customizable properties such as controller, hintText, obscureText, max, and min.
    
    The Button component is a customizable button with properties for text, textColor, buttonColor, buttonWidth, buttonHeight, and onTap.
    
    The TextAlignmentLeft component aligns text to the left with customizable properties for text, textSize, and textColor.
    
    These components will be used in the LoginPage widget to create a login form with email and password input fields, a "Forgot your password?" link, and a login button.

---
+++ lib/login_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/components/input_text';
+import 'package:pomodoro_timer/components/text_alignment.dart';
+import 'package:pomodoro_timer/components/button.dart';
+
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+
+  const LoginPage({super.key, this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  // void logUserIn() async {
+  //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //       email: emailController.text, password: passwordController.text);
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   SystemChrome.setPreferredOrientations([
+  //     DeviceOrientation.portraitUp,
+  //     DeviceOrientation.portraitDown,
+  //     DeviceOrientation.landscapeLeft,
+  //     DeviceOrientation.landscapeRight,
+  //   ]);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+          child: Center(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children:[Padding(
+                padding: const EdgeInsets.only(right: 40),
+                child: Text('UVADORO', style: TextStyle(fontSize: 20, color: Theme.of(context).secondaryHeaderColor),),
+              )],
+            ),
+            const SizedBox(height: 30),
+            Text("Log in", style: TextStyle(fontSize: 30, color: Theme.of(context).secondaryHeaderColor)),
+            const SizedBox(height: 15),
+            TextAlignmentLeft(text: "Email address", textColor: Theme.of(context).secondaryHeaderColor,),
+            const SizedBox(height: 9),
+            InputText(
+                controller: emailController,
+                hintText: "Your email",
+                obscureText: false),
+            const SizedBox(height: 15),
+            TextAlignmentLeft(text: "Password", textColor: Theme.of(context).secondaryHeaderColor),
+            const SizedBox(height: 9),
+            InputText(
+              controller: passwordController,
+              hintText: "Your password",
+              obscureText: false),
+            const SizedBox(height: 9),
+            TextAlignmentLeft(
+             text: "Forgot your password?",
+             textSize: 18,
+             textColor: Theme.of(context).secondaryHeaderColor,
+             ),
+            const SizedBox(
+              height: 15,
+            ),
+            Button(
+             onTap: () {},
+             ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account? ",
+                  style: TextStyle(fontSize: 16, color: Theme.of(context).secondaryHeaderColor),
+                ),
+                GestureDetector(
+                    onTap: widget.onTap,
+                    child: Text(
+                      "Sign up",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Theme.of(context).secondaryHeaderColor),
+                    )),
+              ],
+            )
+          ]),
+        ),
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/login_page.dart';
 import 'package:pomodoro_timer/pomodoro_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  PomodoroPage(),
+      home:  LoginPage(),
     );
   }
 }
